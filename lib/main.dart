bb6983962f7819c194115d5b99d6081ab3c0fd87
tomoyuki.kobayashi <kobatommy@gmail.com>
2020-08-26 15:12:53 +0900

    auth WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import './signIn.dart';
 import './vote.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool loggedIn;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO detect initial vue this hard coded flag;
+    this.setState(() {
+      loggedIn = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Your Favorite Programming Language',
-      home: MyVotePage(),
+      home: loggedIn ? MyVotePage() : MySignInPage(),
     );
   }
 }
